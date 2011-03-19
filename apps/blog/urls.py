8611ce246109ae814b05467d0e128fe06e7d6a08
Martin Ogden <martin.ogden@gmail.com>
2011-03-19 11:38:44 +0000

    Style changes
     * Add mobile styles
    
     * Alter index header

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic import dates
+from django.db import models
 
 from blog.models import Article
 from blog.feeds import LatestArticlesFeed
@@ -8,13 +9,16 @@ urlpatterns = patterns('',
     url(r'$^',
         dates.ArchiveIndexView.as_view(**{
             'date_field': 'published_at',
+            'paginate_by': 10,
+            'allow_empty': True,
             'model': Article}),
     name='home'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
         dates.DateDetailView.as_view(**{
             'date_field': 'published_at',
-            'model': Article,
+            'queryset': Article.objects.all()\
+                .annotate(models.Count('tags')),
             'month_format': '%m'}),
     name='article_detail'),
 
--- settings/development.py
+++ settings/development.py
@@ -8,3 +8,7 @@ DATABASES = {
         'NAME': SITE_ROOT / 'db/development.sqlite3',
     }
 }
+
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS += ('debug_toolbar',)
+INTERNAL_IPS = ('127.0.0.1', '0.0.0.0')
